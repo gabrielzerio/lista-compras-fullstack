fabba641015b2c43192badc1cbfc8db111960dee
Gabriel Zério <21043296+gabrielzerio@users.noreply.github.com>
2024-10-02 13:49:08 -0300

    testando useContext

--- src/front-end/App.jsx
+++ src/front-end/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { createContext } from "react";
+
 import Login from "./login/index.jsx";
 import ListaCompra from "./lista/listaCompras.jsx";
-function App(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/lista-compra" element={<ListaCompra/>}/>
-            </Routes>
-        </BrowserRouter>
+const ThemeContext = createContext("VERDÃO");
+function App() {
+    return (
+        <ThemeContext.Provider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/lista-compra" element={<ListaCompra />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeContext.Provider>
     )
 }
 
--- src/front-end/login/index.jsx
+++ src/front-end/login/index.jsx
@@ -2,30 +2,32 @@ import { useRef } from "react";
 import handleLogin from '../scripts/scriptLogin.js';
 import { LoginModel } from "../objetos/itemModel.js";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 function LoginPage() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const navigate = useNavigate();
-
-  async function handleSubmit(e){
+  const theme = useContext(ThemeContext);
+  console.log(theme);
+  async function handleSubmit(e) {
     e.preventDefault();
     const userLogin = new LoginModel(emailRef.current.value, passwordRef.current.value);
     const resultado = await handleLogin(userLogin);
-    if(resultado == null){
+    if (resultado == null) {
       return;
     }                       //tratar os responses de forma correta
-    localStorage.setItem('token',resultado);
+    localStorage.setItem('token', resultado);
     navigate("/lista-compra");
-   }
+  }
 
   return (
     <div className="max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-lg ">
-        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Login</h2>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4"> {/*flex flex-wrap flex-col*/}
-            <input ref={emailRef} placeholder="Usuario" type="email" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
-            <input ref={passwordRef} placeholder="Senha" type="password" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
-            <button type="submit" className="w-full bg-blue-500 rounded-md py-2 px-4 hover:bg-blue-300 font-medium">Login</button>
-        </form>      
+      <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Login</h2>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4"> {/*flex flex-wrap flex-col*/}
+        <input ref={emailRef} placeholder="Usuario" type="email" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <input ref={passwordRef} placeholder="Senha" type="password" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none" />
+        <button type="submit" className="w-full bg-blue-500 rounded-md py-2 px-4 hover:bg-blue-300 font-medium">Login</button>
+      </form>
     </div>
   );
 }
