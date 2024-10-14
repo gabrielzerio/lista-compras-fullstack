22bcda8cbe1fe0acdcf5318537223217b5159569
Gabriel Zério <21043296+gabrielzerio@users.noreply.github.com>
2024-10-14 12:33:56 -0300

    riscando itens comprados (front)

--- src/front-end/lista/item.jsx
+++ src/front-end/lista/item.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
+import React from "react";
 /* eslint-disable react/prop-types */
-export function Item({produto}) {
+export function Item({ produto, teste} ) {
+  const [riscado, setRiscado] = useState(false);
+
+  const toggleRiscado = () => {
+    setRiscado(!riscado);
+  };
+
   const dataLocal = new Date(produto.data).toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" });
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 p-4 bg-white shadow-md rounded-lg border border-gray-300">
-      <div className="p-3 bg-gray-100 text-lg font-semibold rounded">
-      <span className="font-bold"></span>{produto.produto} |  {produto.qtd}
+    <div 
+      onClick={toggleRiscado} 
+      className={`grid grid-cols-2 gap-1 sm:grid-cols-3 sm:gap-4 sm:p-4 bg-white shadow-md rounded-lg border border-gray-300 cursor-pointer
+      ${riscado ? "line-through text-red-400" : ""}`}
+    >
+      <div className="p-3 bg-gray-100 text-lg text-gray-700  font-semibold rounded">
+        {produto.produto} | {produto.qtd}
       </div>
       <div className="p-3 bg-gray-50 text-gray-700 rounded">
-        <span className="font-bold">Qtd: </span> {produto.qtd}
+        {produto.usuario.nome}
       </div>
-      <div className="p-3 bg-gray-50 text-gray-700 rounded">
-        <span className="font-bold">Solicitante: </span> {produto.usuario.nome}
-      </div>
-      <div className="p-3 bg-gray-50 text-gray-700 rounded">
-        <span className="font-bold">Adicionado: </span> {dataLocal}
+      <div className="order-first bg-gray-50 rounded text-center col-span-2 sm:col-span-1 text-gray-700 sm:p-3 sm:text-left sm:order-none">
+        {dataLocal}
       </div>
     </div>
   );
--- src/front-end/lista/listaCompra.jsx
+++ src/front-end/lista/listaCompra.jsx
@@ -1,32 +1,27 @@
 import { useState, useEffect } from "react";
 import { Item } from "./item.jsx";
 import { ItemModel } from "../objetos/itemModel.js";
-import { fetchItens } from "../scripts/scriptListaItens.js";
-import { fetchAllItens } from "../scripts/scriptListaItens.js";
+import { fetchItens, fetchAllItens } from "../scripts/scriptListaItens.js";
 import { adicionaItem } from "../scripts/scriptAdicionaItem.js";
-
+import { useNavigate } from "react-router-dom";
 
 function ListaCompra() {
+  const navigate = useNavigate();
+  
   const token = localStorage.getItem('tkn');
-
-// Separa o payload (a segunda parte do token) e decodifica
-const base64Url = token.split('.')[1];
-const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-const payload = JSON.parse(atob(base64));
-const usuario = payload.nome;
-const id_usuario = payload.id
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const payload = JSON.parse(atob(base64));
+  const usuario = payload.nome;
+  const id_usuario = payload.id;
 
   const [itens, setItens] = useState([]);
   const [nomeProduto, setNomeProduto] = useState("");
   const [qtdProduto, setQtdProduto] = useState(0);
   const [showAll, setShowAll] = useState(false);
 
-  const handleNomeProduto = (e) => {
-    setNomeProduto(e.target.value);
-  };
-  const handleQtdProduto = (e) => {
-    setQtdProduto(e.target.value);
-  };
+  const handleNomeProduto = (e) => setNomeProduto(e.target.value);
+  const handleQtdProduto = (e) => setQtdProduto(e.target.value);
 
   const handleAdicionaProduto = async () => {
     const novoItem = new ItemModel(nomeProduto, parseInt(qtdProduto), "66f708a785ec7101e05e6177", id_usuario, usuario);
@@ -34,37 +29,38 @@ const id_usuario = payload.id
     await handleLista();
   };
 
-  const handleCheckBox = (e) => {
-      setShowAll(e.target.checked)
-  }
+  const handleCheckBox = (e) => setShowAll(e.target.checked);
 
   const handleLista = async () => {
-    if(showAll){
-    const data = await fetchItens(token);
-    setItens(data);
-    }else{
-    const data = await fetchAllItens(token);
-    setItens(data);
+    if(localStorage.getItem('tkn') === ""){
+      navigate('/');
     }
-    
-    // setSolicitante(data['solicitante']);
-  }
-  
+    const data = showAll ? await fetchItens(token) : await fetchAllItens(token);
+    setItens(data);
+  };
+
   useEffect(() => {
+    
     handleLista();
+  }, [showAll]);
 
-    },[showAll])
-
+  function handleQuit(){
+    localStorage.setItem('tkn', "");
+    navigate("/");  
+  }
   return (
     <>
-      <div className="bg-blue-400 p-3 text-lg font-bold">Usuario: {usuario}</div>
+      <div className="bg-blue-400 p-3 text-lg font-bold flex justify-between">
+        <div>Usuario: {usuario}</div>
+        <button onClick={handleQuit}>SAIR</button>
+      </div>
+
       <div className="p-4 bg-gray-50 rounded-lg shadow-md flex flex-col md:flex-row items-start md:items-center gap-4">
-      <label>Meus Itens</label>
-      <input type="checkbox" checked={showAll} onChange={handleCheckBox} name="yn" className="indeterminate:bg-gray-300 w-6 h-6"  />
+        <label>Meus Itens</label>
+        <input type="checkbox" checked={showAll} onChange={handleCheckBox} className="indeterminate:bg-gray-300 w-6 h-6" />
         <input
           className="border border-gray-300 rounded-lg p-2 w-full md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-400"
           type="text"
-          name="input-produto"
           placeholder="Produto"
           value={nomeProduto}
           onChange={handleNomeProduto}
@@ -72,7 +68,6 @@ const id_usuario = payload.id
         <input
           className="border border-gray-300 rounded-lg p-2 w-full md:w-auto focus:outline-none focus:ring-2 focus:ring-blue-400"
           type="number"
-          name="input-qtd"
           placeholder="Quantidade"
           value={qtdProduto}
           onChange={handleQtdProduto}
@@ -84,11 +79,13 @@ const id_usuario = payload.id
           Adicionar
         </button>
       </div>
-      { itens.map((produto) => (
-        <Item key={produto.id} produto={produto} solicitante={produto.id_usuario}  />
+      {itens.map((produto) => (
+        <div key={produto.id} className="fade-in-up">
+          <Item produto={produto} />
+        </div>
       ))}
-      
     </>
   );
 }
+
 export default ListaCompra;
